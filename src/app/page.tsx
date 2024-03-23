b1bd87e83ad57552c5787b32e58566198d52beab
avi <avi.contact22@gmail.com>
2024-03-23 14:23:11 +0530

    Added login/signup button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>Todo App</div>;
+  return (
+    <div>
+      <h3>Todo app</h3>
+      <Link href={"/signup"}>
+        <button className="bg-blue-200 mr-2">Signup</button>
+      </Link>
+      <Link href={"/login"}>
+        <button className="bg-red-200">Login</button>
+      </Link>
+    </div>
+  );
 }
